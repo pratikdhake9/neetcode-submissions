bbe0e1227332cfc8e00c8f24c9ee3a443b113591
Pratik Dhake <83688851+pratikdhake9@users.noreply.github.com>
2026-08-23 20:52:15 +0530

    Bulk sync: 3 submissions

---
+++ SQL/sql-create-table/submission-1.sql
@@ -0,0 +1,18 @@
+
+create table videos (
+ id Integer,
+ name TEXT,
+ created_at date,
+ published BOOLEAN 
+
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
